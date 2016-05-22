890f290bf8585ddee88893702e050c4f93fc4def
GraceSherin12 <gracesherinkandula@gmail.com>
2016-05-23 00:37:28 +0530

    Hi im trying first

---
+++ javademo/src/javademo/firstprg.java
@@ -0,0 +1,10 @@
+package javademo;
+
+public class firstprg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Program has run successfully");
+	}
+
+}
